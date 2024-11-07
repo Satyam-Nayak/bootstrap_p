2368cbdc94aa580efd23e5920f07d1b56683d261
Satyam-Nayak <satyamn120@gmail.com>
2024-11-07 12:49:42 +0530

    new bootstarp added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Compo1 from './Compo1';
 import BasicExample from './Compo2';
 import Compo3 from './Compo3';
 import Compo4 from './Compo4'; // Adjust the path if needed
+import Compo5 from './Compo5';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
       <Compo3/>
       <br /><br /><br /><br />
       <Compo4/>
+      <br /><br /><br />
+      <Compo5/>
     </div>
   );
 }
---
+++ src/Compo5.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState, useRef } from 'react';
+import Button from 'react-bootstrap/Button';
+import Overlay from 'react-bootstrap/Overlay';
+function Compo5(){
+    const [show, setShow] = useState(false);
+  const target = useRef(null);
+return(
+<>
+<Button variant="danger" ref={target} onClick={() => setShow(!show)}>
+        Click me to see
+      </Button>
+      <Overlay target={target.current} show={show} placement="right">
+        {({
+          placement: _placement,
+          arrowProps: _arrowProps,
+          show: _show,
+          popper: _popper,
+          hasDoneInitialMeasure: _hasDoneInitialMeasure,
+          ...props
+        }) => (
+          <div
+            {...props}
+            style={{
+              position: 'absolute',
+              backgroundColor: 'rgba(255, 100, 100, 0.85)',
+              padding: '2px 10px',
+              color: 'white',
+              borderRadius: 3,
+              ...props.style,
+            }}
+          >
+            Simple tooltip
+          </div>
+        )}
+
+      </Overlay>
+</>
+
+    )
+
+}
+export default Compo5
